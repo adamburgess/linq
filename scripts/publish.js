4a68dfb7f2141f08c641fc6bf409c3fe9e78346c
Adam Burgess <adamburgess@users.noreply.github.com>
2021-03-22 16:59:36 +1100

    publish scripts

---
+++ scripts/publish.js
@@ -0,0 +1,6 @@
+import fs from 'fs/promises'
+const pkgjson = 'publish/package.json';
+const pkg = JSON.parse(await fs.readFile(pkgjson, 'utf8'));
+// delete stuff we don't need.
+delete pkg.scripts;
+await fs.writeFile(pkgjson, JSON.stringify(pkg, null, 2));
